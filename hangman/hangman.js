1c599d17ca3045abc838c87c8eaaf21f1a0541c2
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-15 15:07:41 +0300

    the class syntax

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,65 +1,65 @@
-const Hangman = function (word, guessNum) {
-  this.word = word.toLowerCase().split("");
-  this.guessNum = guessNum;
-  this.guessedLetters = [];
-  this.status = "playing";
-};
-
-Hangman.prototype.calculateStatus = function () {
-  const finished = this.word.every((letter) =>
-    this.guessedLetters.includes(letter)
-  );
-
-  if (this.guessNum === 0) {
-    this.status = "failed";
-  } else if (finished) {
-    this.status = "finished";
-  } else {
+class Hangman {
+  constructor(word, guessNum) {
+    this.word = word.toLowerCase().split("");
+    this.guessNum = guessNum;
+    this.guessedLetters = [];
     this.status = "playing";
   }
-};
+  calculateStatus() {
+    const finished = this.word.every((letter) =>
+      this.guessedLetters.includes(letter)
+    );
 
-Hangman.prototype.getPuzzle = function () {
-  let puzzle = "";
-
-  this.word.forEach((letter) => {
-    if (this.guessedLetters.includes(letter) || letter === " ") {
-      puzzle += letter;
+    if (this.guessNum === 0) {
+      this.status = "failed";
+    } else if (finished) {
+      this.status = "finished";
     } else {
-      puzzle += "*";
+      this.status = "playing";
     }
-  });
-
-  return puzzle;
-};
-
-Hangman.prototype.getStatusMessage = function () {
-  if (this.status === "playing") {
-    return `Guesses left: ${this.guessNum}`;
-  } else if (this.status === "failed") {
-    return `Nice try! The word was "${this.word.join("")}".`;
-  } else {
-    return "Great work! You guessed the work.";
   }
-};
+  getPuzzle() {
+    let puzzle = "";
 
-Hangman.prototype.makeGuess = function (guess) {
-  guess = guess.toLowerCase();
-  const isUnique = !this.guessedLetters.includes(guess);
-  const isBadGuess = !this.word.includes(guess);
+    this.word.forEach((letter) => {
+      if (this.guessedLetters.includes(letter) || letter === " ") {
+        puzzle += letter;
+      } else {
+        puzzle += "*";
+      }
+    });
 
-  // Disable new guess unless 'playing'
-  if (this.status !== "playing") {
-    return;
+    return puzzle;
   }
 
-  if (isUnique) {
-    this.guessedLetters.push(guess);
+  getStatusMessage() {
+    if (this.status === "playing") {
+      return `Guesses left: ${this.guessNum}`;
+    } else if (this.status === "failed") {
+      return `Nice try! The word was "${this.word.join("")}".`;
+    } else {
+      return "Great work! You guessed the work.";
+    }
   }
 
-  if (isUnique && isBadGuess) {
-    this.guessNum--;
-  }
+  makeGuess(guess) {
+    guess = guess.toLowerCase();
+    const isUnique = !this.guessedLetters.includes(guess);
+    const isBadGuess = !this.word.includes(guess);
 
-  this.calculateStatus();
-};
+    // Disable new guess unless 'playing'
+    if (this.status !== "playing") {
+      return;
+    }
+
+    if (isUnique) {
+      this.guessedLetters.push(guess);
+    }
+
+    if (isUnique && isBadGuess) {
+      this.guessNum--;
+    }
+
+    this.calculateStatus();
+  }
+}
