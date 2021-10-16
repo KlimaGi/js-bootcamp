67dd705e1587449549143593a48869027d0d3c52
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-16 22:59:37 +0300

    integrating data into the application

--- hangman/app.js
+++ hangman/app.js
@@ -1,42 +1,40 @@
 const puzzleEl = document.querySelector("#puzzle");
 const guessNumEl = document.querySelector("#guess-num");
-const game1 = new Hangman("chess game", 4);
-
-puzzleEl.textContent = game1.puzzle;
-guessNumEl.textContent = game1.statusMessage;
+let game1;
 
 window.addEventListener("keypress", (e) => {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
-
-  puzzleEl.textContent = game1.puzzle;
-  guessNumEl.textContent = game1.guessNum;
-  guessNumEl.textContent = game1.statusMessage;
+  render();
 });
 
-getPuzzle("2")
-  .then((puzzle) => {
-    console.log(puzzle);
-  })
-  .catch((err) => {
-    console.log(`Error: ${err}`);
-  });
+const render = () => {
+  puzzleEl.textContent = game1.puzzle;
+  guessNumEl.textContent = game1.statusMessage;
+};
 
-getCurrentCountry()
-  .then((country) => {
-    console.log(country.name);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const startGame = async () => {
+  const puzzle = await getPuzzle("2");
+  game1 = new Hangman(puzzle, 5);
+  render();
+};
 
-// getLocation()
-//   .then((data) => {
-//     return getCountry(data.country);
-//   })
-//   .then((country) => {
-//     console.log(country.name);
+document.querySelector("#reset").addEventListener("click", startGame);
+
+startGame();
+
+// getPuzzle("2")
+//   .then((puzzle) => {
+//     console.log(puzzle);
 //   })
 //   .catch((err) => {
 //     console.log(`Error: ${err}`);
+//   });
+
+// getCurrentCountry()
+//   .then((country) => {
+//     console.log(country.name);
+//   })
+//   .catch((error) => {
+//     console.log(error);
 //   });
