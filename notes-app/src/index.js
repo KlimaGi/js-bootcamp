98a8bdaf2f0c21ec9e3dc1d25bc6b50517334812
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-20 15:16:00 +0300

    converting notes app -3

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,5 +1,18 @@
-import { getNotes, createNote } from "./notes";
+import { getNotes, createNote, removeNote, updateNote } from "./notes";
+import { getFilters, setFilters } from "./filters";
 
-console.log(getNotes());
-createNote();
-console.log(getNotes());
+// console.log(getNotes());
+// createNote();
+//removeNote();
+// updateNote("42ce5d25-bdb6-4218-a00d-54869d865172", {
+//   title: "My note title",
+//   body: "New note body.",
+// });
+// console.log(getNotes());
+
+console.log(getFilters());
+setFilters({
+  searchText: "Office",
+  sortBy: "byCreated",
+});
+console.log(getFilters());
