88fe46aa55bd02e287754050512572f25d532c13
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-20 23:44:44 +0300

    the spread syntax

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,12 +1,12 @@
-const calculateAverage = (thing, ...numbers) => {
-  //return (numOne + numTwo) / 2;
-  let sum = 0;
-  numbers.forEach((num) => (sum += num));
-  const average = sum / numbers.length;
-  return `The average ${thing} is ${average}`;
-};
+// const calculateAverage = (thing, ...numbers) => {
+//   //return (numOne + numTwo) / 2;
+//   let sum = 0;
+//   numbers.forEach((num) => (sum += num));
+//   const average = sum / numbers.length;
+//   return `The average ${thing} is ${average}`;
+// };
 
-console.log(calculateAverage("grade", 0, 100, 88, 64));
+// console.log(calculateAverage("grade", 0, 100, 88, 64));
 
 const printTeam = (team, coach, ...players) => {
   console.log(`Team: ${team}`);
@@ -14,4 +14,15 @@ const printTeam = (team, coach, ...players) => {
   console.log(`Players: ${players.join(", ")}`);
 };
 
-printTeam("Liberty", "Casey Penn", "Marge", "Aiden", "Herbert", "Sherry");
+const team = {
+  name: "Liberty",
+  coach: "Casey Penn",
+  players: ["Marge", "Aiden", "Herbert", "Sherry"],
+};
+
+printTeam(team.name, team.coach, ...team.players);
+
+let cities = ["Barcelona", "Cape Town", "Bordeaux"];
+cities = [...cities, "Amsterdam"];
+
+console.log(cities);
--- hangman/src/hangman.js
+++ hangman/src/hangman.js
@@ -53,7 +53,7 @@ class Hangman {
     }
 
     if (isUnique) {
-      this.guessedLetters.push(guess);
+      this.guessedLetters = [...this.guessedLetters, guess];
     }
 
     if (isUnique && isBadGuess) {
